59c084c4757081f509b5218b6e5a5c1d1b31e723
MrSullivan <>
2021-05-30 21:10:49 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,22 @@
 def main():
     #write your code below this line
+    input("Name of the file:")
+    
+    with open("names.txt",'r') as f:
+      data = f.read().splitlines()  
+    
+    name = input("Enter names, an empty line quits.")
+    while True:
+      if(name == ""):
+        break
+      elif(name in data):
+        print("The name is on the list.")
+        name = input("Enter names, an empty line quits.")
+      else:
+        print("The name is not on the list.")
+        name = input("Enter names, an empty line quits.")
+      
+    print("Thank you!")
 
 if __name__ == '__main__':
     main()
